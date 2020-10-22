eb67c9ab2b509115c7b7071eaa09d509b0559884
zemin xu <zemin.xu@yahoo.com>
2020-10-22 15:13:47 +0200

    sift done but with too much match

--- Main.cpp
+++ Main.cpp
@@ -8,10 +8,15 @@
 using namespace std;
 using namespace cv;                        // OpenCV classes and routines
 
-Mat frame, frame_gray, frame_gray32f, frame_smoothed, frame_overlay;
-Mat* the_frame;
+Mat img_example;
 
-vector<KeyPoint> keypoints;
+Mat frame, frame_gray, frame_gray32f, frame_smoothed, frame_overlay;
+
+Mat img_matches;
+
+Mat descriptors1, descriptors2;
+
+vector<KeyPoint> keypoints1, keypoints2;
 
 VideoCapture cap;
 bool isGrayCamera = false;
@@ -26,6 +31,14 @@ void update_frame();
 int main(int argc, char* argv[])
 {
 	//create_GUI();
+
+	string image_path = samples::findFile("poemes.jpg");
+	img_example = imread(image_path, IMREAD_COLOR);
+	if (img_example.empty())
+	{
+		std::cout << "Could not read the image: " << image_path << std::endl;
+		return 1;
+	}
 
 	cap.open(0);
 	cap >> frame;
@@ -46,63 +59,26 @@ int main(int argc, char* argv[])
 
 void create_GUI()
 {
-	/*
-	Mat img1 = imread("box.png", IMREAD_GRAYSCALE);
-	Mat img2 = imread("box_in_scene.png", IMREAD_GRAYSCALE);
-
-	Mat image_out = img1;
-	Mat descriptor;
-
-	Ptr<ORB> orb = ORB::create(1);
-	orb->detectAndCompute(img1, Mat(), keypoints, descriptor);
-
-	// Overlay keypoints onto original images
-	drawKeypoints(img1, keypoints, image_out,
-		KPColor, DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	//	imshow(window_out_name, image_out);
-
-	//-- Step 1: Detect the keypoints using SURF Detector
-	Ptr<SIFT> detector = SIFT::create();
-	vector<KeyPoint> kps;
-	Mat img_gray;
-	cvtColor(img2, img_gray, COLOR_BGR2GRAY);
-	detector->detect(img_gray, kps);
-	//-- Draw keypoints
-	drawKeypoints(img2, kps, image_out);
-	//-- Show detected (drawn) keypoints
-	imshow("SIFT Keypoints", image_out);
-	*/
 }
 
 void update_frame()
 {
-	vector<KeyPoint> keypoints1, keypoints2;
-
 	cap >> frame;
 
 	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
 
 	/* feature detection */
 	Ptr<SIFT> siftPtr = SIFT::create();
-	//siftPtr->detect(frame_gray, keypoints);
-	//drawKeypoints(frame, keypoints, frame);
 
-	//-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-	Mat descriptors1, descriptors2;
-	siftPtr->detectAndCompute(frame, noArray(), keypoints1, descriptors1);
+	siftPtr->detectAndCompute(img_example, noArray(), keypoints1, descriptors1);
 	siftPtr->detectAndCompute(frame, noArray(), keypoints2, descriptors2);
 
 	/* feature description */
-	//-- Step 2: Matching descriptor vectors with a brute force matcher
-	// Since SURF is a floating-point descriptor NORM_L2 is used
 	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
-	std::vector< DMatch > matches;
+	vector<DMatch> matches;
 	matcher->match(descriptors1, descriptors2, matches);
 	//-- Draw matches
-	Mat img_matches;
-	drawMatches(frame, keypoints1, frame, keypoints2, matches, img_matches);
+	drawMatches(img_example, keypoints1, frame, keypoints2, matches, img_matches);
 	//-- Show detected matches
-	imshow("Matches", img_matches);
-
-	//imshow(window_out_name, frame);
+	imshow(window_out_name, img_matches);
 }
